959ce315a1f7c5fadf02472926ea7d5a2dda0a87
unknown <krishsonar3007@gmail.com>
2026-07-12 11:39:34 +0530

    maintainance

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Sidebar from "./components/Sidebar"
 import DriverManagement from "./pages/DriverManagement"
+import Maintenance from "./pages/Maintainance"
 
 const App=()=>{
   return <div>
     <Navbar/>
     <Routes>
       <Route path='/drivers' element={<DriverManagement/>} />
+      <Route path='/maintainance' element={<Maintenance />} />
     </Routes>
   </div>
 }
